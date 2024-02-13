9edc5c83994b483adc08ef5778635da88849438b
vdc <vanderson.castro@ufv.br>
2024-02-13 18:01:07 -0300

    init 130324

---
+++ python basic/teste.py
@@ -0,0 +1,7 @@
+
+name = input('Qual é o seu nome ')
+idade = str(input('qual é sua idade '))
+cidade = input('Onde vocẽ mora ')
+
+print(f'Okay {name}, vocẽ tem {idade} anos '
+      f'e mora em {cidade}.')
